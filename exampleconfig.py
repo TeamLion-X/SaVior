2a3b3a4a277b8f3dbc627974142fb8dc54d7b6d8
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-05-08 08:53:51 +0530

    Add files via upload

--- exampleconfig.py
+++ exampleconfig.py
@@ -10,12 +10,13 @@ class Development(Config):
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "Your value"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
-    STRING_SESSION = "Your value"
+    SAVIOR_STRING = "Your value"
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "Your value"
-    # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -100
+    BOT_TOKEN = "Your value"
     # command handler
-    COMMAND_HAND_LER = "."
-    # command hanler for sudo
-    SUDO_COMMAND_HAND_LER = "."
+    HANDLER = "."
+    # spam of assistant
+    SPAM = "Your Value"
+    UPSTREAM_REPO = "pro"
+    # Your City's TimeZone
+    TZ = "Your value"
